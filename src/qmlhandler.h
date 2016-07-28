bd0747885c17cce7787346bbfafc4b7633254c0e
alexey192 <alexey192@gmail.com>
2016-07-29 00:28:02 +0300

    new qmlhandler data model

--- src/qmlhandler.h
+++ src/qmlhandler.h
@@ -2,23 +2,23 @@
 #define QMLHANDLER_H
 
 #include <QObject>
-#include <QListData>
-#include <QAbstractListModel>
+//#include <QListData>
+//#include <QAbstractListModel>
 
 class QmlHandler : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY (QAbstractListModel routeModel READ routeModel WRITE setRouteModel NOTIFY routeModelChanged)
+    Q_PROPERTY (QList<QObject*> routeModel READ routeModel WRITE setRouteModel NOTIFY routeModelChanged)
 public:
     explicit QmlHandler(QObject *parent = 0);
 
-    void setRouteModel(const QAbstractListModel &newRtModel)
+    void setRouteModel(const QList<QObject*> &newRtModel)
     {
         m_routeModel = newRtModel;
         emit routeModelChanged();
     }
 
-    QAbstractListModel routeModel() const
+    QList<QObject*> routeModel() const
     {
         return m_routeModel;
     }
@@ -26,7 +26,7 @@ signals:
     void routeModelChanged();
 public slots:
 private:
-    QAbstractListModel m_routeModel;
+    QList<QObject*> m_routeModel;
 };
 
 #endif // QMLHANDLER_H
